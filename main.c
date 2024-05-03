50825b177f6ea5d4250dcbd62a2a92c9fb3d6ece
mkadri66 <mustapha.kadri66@gmail.com>
2024-05-03 21:12:15 +0000

    create pipe - fork structure

--- main.c
+++ main.c
@@ -6,15 +6,41 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:29:45 by mkadri            #+#    #+#             */
-/*   Updated: 2024/04/17 14:49:24 by mkadri           ###   ########.fr       */
+/*   Updated: 2024/05/03 21:05:37 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int main(int argc, char **argv)
+int main(int argc, char **argv, char **env_vars)
 {
-    count_args(argc);
-    (void) argv;
-    return(0);
+	int pipe_fd[2];
+	pid_t pid;
+
+	count_args(argc);
+	if(pipe(pipe_fd) == -1)
+	{
+		perror("Pipe error");
+		exit(EXIT_FAILURE);
+	}
+	pid = fork();
+	if(pid == -1)
+	{
+		perror("Fork error");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		/* Child process*/
+		ft_printf("Child \n");
+	}
+	else
+	{
+		/* Parent process*/
+		wait(NULL);
+		ft_printf("Parent \n");
+	}
+	(void) argv;
+	(void) env_vars;
+	return(0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: mkadri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:31:52 by mkadri            #+#    #+#             */
-/*   Updated: 2024/04/17 14:48:57 by mkadri           ###   ########.fr       */
+/*   Updated: 2024/05/03 21:06:05 by mkadri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 # include <stdio.h>
 # include <fcntl.h>
 # include <stdbool.h>
+#include <sys/wait.h>
 
 int  count_args(int argc);
 
